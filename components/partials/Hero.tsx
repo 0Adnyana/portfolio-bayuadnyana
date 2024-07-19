7d7fcf4232a6792dab3af5a7e5c8c591d6b880c4
0Adnyana <87359319+0Adnyana@users.noreply.github.com>
2024-07-19 16:27:58 +0800

    finalized Hero.tsx

--- components/partials/Hero.tsx
+++ components/partials/Hero.tsx
@@ -1,9 +1,11 @@
 import { GrGithub, GrInstagram, GrLinkedin, GrMail } from "react-icons/gr";
 import NavLink from "../ui/NavLink";
+import Image from "next/image";
+import ProfilePicture from "@/public/image.png"
 
 const Hero = () => {
   return (
-    <section className="md:w-[70%] mx-auto flex items-center justify-between">
+    <section className="md:w-[70%] mx-auto flex flex-row items-center justify-between">
       <div className="flex flex-col space-y-4">
 
         {/* Name and Description */}
@@ -17,14 +19,19 @@ const Hero = () => {
         <div className="flex flex-wrap space-x-4">
           <a href="https://github.com/0Adnyana"><GrGithub className="text-foreground size-6 hover:scale-110 cursor-pointer ease-in-out duration-200" ></GrGithub></a>
           <a href="https://instagram.com/bayu.adnyana0"><GrInstagram className="text-foreground size-6 hover:scale-110 cursor-pointer ease-in-out duration-200"></GrInstagram></a>
-          <a href="https://linkedin.com/bayuadnyana"><GrLinkedin className="text-foreground size-6 hover:scale-110 cursor-pointer ease-in-out duration-200"></GrLinkedin></a>
+          <a href="https://linkedin.com/in/bayuadnyana"><GrLinkedin className="text-foreground size-6 hover:scale-110 cursor-pointer ease-in-out duration-200"></GrLinkedin></a>
           <a href="https://bayup.adnyana@gmail.com"><GrMail className="text-foreground size-6 hover:scale-110 cursor-pointer ease-in-out duration-200"></GrMail></a>
         </div>
-        
       </div>
 
-      <div>
-        
+      {/* Profile Picture */}
+      <div className="group sm:size-56 md:size-56 md:shrink-0 relative flex items-center justify-center">
+        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[80%] h-[75%] border-2 border-foreground border-b-0 rounded-xl bg-opacity-0 z-0 group-hover:border-primary ease-in-out duration-200"></div>
+        <Image 
+          src={ProfilePicture} 
+          alt="profile picture" 
+          className="z-10 ease-in-out duration-300 brightness-[90%] group-hover:scale-[104%]" 
+        />
       </div>
     </section>
   )
