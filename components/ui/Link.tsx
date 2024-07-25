590ef0935b555692fb5ee29d322d1cd136c715b0
0Adnyana <87359319+0Adnyana@users.noreply.github.com>
2024-07-25 17:28:14 +0800

    update css

--- components/ui/Link.tsx
+++ components/ui/Link.tsx
@@ -7,7 +7,7 @@ type LinkProps = {
 
 const Link = (props: LinkProps) => {
   return (
-    <a className="flex flex-row items-center italic underline font-roboto font-normal text-base -translate-y-0.5 hover:brightness-75 ease-in-out duration-200" href={props.link} target="_blank" rel="noopener noreferrer">{props.children} <RiExternalLinkLine className="ml-0.5 size-4 translate-y-0.5"></RiExternalLinkLine></a>
+    <a className="flex flex-row items-center italic underline font-roboto font-normal text-base hover:brightness-75 ease-in-out duration-200" href={props.link} target="_blank" rel="noopener noreferrer">{props.children} <RiExternalLinkLine className="ml-1 size-4"></RiExternalLinkLine></a>
   )
 }
 
