fa9385eb956aec9928d46ff82ce71d56c8730e75
0Adnyana <87359319+0Adnyana@users.noreply.github.com>
2024-07-26 16:04:38 +0800

    update css

--- components/ui/SectionTitle.tsx
+++ components/ui/SectionTitle.tsx
@@ -4,7 +4,7 @@ type SectionTitleProps = {
 
 const SectionTitle = (props: SectionTitleProps) => {
   return (
-    <h2 className="font-roboto_mono font-bold text-2xl text-foreground">{props.title}</h2>
+    <h2 className="font-roboto_mono font-bold text-2xl text-foreground pb-4">{props.title}</h2>
   )
 }
 
--- components/ui/SocialLink.tsx
+++ components/ui/SocialLink.tsx
@@ -1,11 +1,12 @@
 type SocialLinkProps = {
     href: string;
     children: React.ReactNode;
+    className?: string;
 }
 
 const SocialLink = (props: SocialLinkProps) => {
   return (
-    <a href={props.href} target="_blank" rel="noopener noreferrer" className="text-foreground size-6 hover:scale-110 cursor-pointer ease-in-out duration-200">{props.children}</a>
+    <a href={props.href} target="_blank" rel="noopener noreferrer" className={`text-foreground hover:scale-110 cursor-pointer ease-in-out duration-200 ${props.className}`}>{props.children}</a>
   )
 }
 
