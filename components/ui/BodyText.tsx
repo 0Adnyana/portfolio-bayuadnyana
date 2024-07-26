ed831b78d322ef1c0fcde5e69ae1b7a7141cacde
0Adnyana <87359319+0Adnyana@users.noreply.github.com>
2024-07-26 16:03:20 +0800

    Create BodyText.tsx

---
+++ components/ui/BodyText.tsx
@@ -0,0 +1,12 @@
+type BodyTextProps = {
+    className?: string;
+    children: React.ReactNode;
+}
+
+const BodyText = (props: BodyTextProps) => {
+  return (
+    <p className={`font-roboto font-normal text-base text-justify  ${props.className}`}>{props.children}</p>
+  )
+}
+
+export default BodyText
