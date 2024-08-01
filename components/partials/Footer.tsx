9059785080c23c03bc5e35326e22c8bbdeb34294
0Adnyana <87359319+0Adnyana@users.noreply.github.com>
2024-08-01 18:20:22 +0800

    Create Footer.tsx

---
+++ components/partials/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="border-t-[1px] border-background-lighter">
+        <div className="md:w-[70%] mx-auto flex items-center justify-center py-2 bg-background">
+            <p className="font-roboto text-sm text-foreground">
+                Copyright © 2024 I Made Bayu Putra Adnyana. All rights reserved.
+            </p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
