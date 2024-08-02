4e9923e13cc2f95e25d2bbf5702fecc750a2fb6f
0Adnyana <87359319+0Adnyana@users.noreply.github.com>
2024-08-02 17:49:47 +0800

    updated contents

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/partials/Header";
 import Hero from "@/components/partials/Hero";
 import AboutMe from "@/components/partials/AboutMe";
-import PlaceholderImage from "@/public/assets/placeholderImage.jpg";
+import AboutMeImage from "@/public/assets/aboutme_image.png";
 import ExperienceSection from "@/components/partials/ExperienceSection";
 import PortfolioSection from "@/components/partials/PortfolioSection"
 import ContactSection from "@/components/partials/ContactSection";
@@ -15,7 +15,7 @@ export default function Home() {
         <Hero></Hero>
         <AboutMe
           content="Lorem ipsum dolor sit amet consectetur. Aliquet tincidunt viverra nulla pretium morbi mattis viverra nibh at. A morbi risus proin ultrices donec. Tortor viverra feugiat id nibh gravida. Gravida et dui justo eleifend dictum placerat mus sed vulputate. Amet in amet tempor pretium nulla mauris eros ultricies. Aliquam quam ornare ornare non gravida imperdiet quis sed. Erat condimentum sit commodo ultrices."
-          imgSrc={PlaceholderImage}
+          imgSrc={AboutMeImage}
           imgAlt="placeholder image"/>
         <ExperienceSection></ExperienceSection>
         <PortfolioSection></PortfolioSection>
