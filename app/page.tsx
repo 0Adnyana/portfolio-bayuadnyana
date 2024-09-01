4c94991a4e3236b5e7c430a427a7bb489d45da98
0Adnyana <87359319+0Adnyana@users.noreply.github.com>
2024-09-01 21:41:03 +0800

    new portfolio data

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,24 @@
 import Header from "@/components/partials/Header";
 import Hero from "@/components/partials/Hero";
 import AboutMe from "@/components/partials/AboutMe";
-import AboutMeImage from "@/public/assets/aboutme_image.png";
+
 import ExperienceSection from "@/components/partials/ExperienceSection";
-import PortfolioSection from "@/components/partials/PortfolioSection"
+import PortfolioSection from "@/components/partials/PortfolioSection";
 import ContactSection from "@/components/partials/ContactSection";
 import Footer from "@/components/partials/Footer";
 
 export default function Home() {
-  return (
-    <div className="fill-background pt-36">
-      <Header></Header>
-      <section className="space-y-24 h-full pb-12">
-        <Hero></Hero>
-        <AboutMe
-          content="Lorem ipsum dolor sit amet consectetur. Aliquet tincidunt viverra nulla pretium morbi mattis viverra nibh at. A morbi risus proin ultrices donec. Tortor viverra feugiat id nibh gravida. Gravida et dui justo eleifend dictum placerat mus sed vulputate. Amet in amet tempor pretium nulla mauris eros ultricies. Aliquam quam ornare ornare non gravida imperdiet quis sed. Erat condimentum sit commodo ultrices."
-          imgSrc={AboutMeImage}
-          imgAlt="placeholder image"/>
-        <ExperienceSection></ExperienceSection>
-        <PortfolioSection></PortfolioSection>
-        <ContactSection></ContactSection>
-      </section>
-      <Footer></Footer>
-    </div>
-  );
+	return (
+		<div className="fill-background pt-36">
+			<Header></Header>
+			<section className="space-y-24 h-full pb-12">
+				<Hero></Hero>
+				<AboutMe />
+				<ExperienceSection></ExperienceSection>
+				<PortfolioSection></PortfolioSection>
+				<ContactSection></ContactSection>
+			</section>
+			<Footer></Footer>
+		</div>
+	);
 }
--- components/partials/AboutMe.tsx
+++ components/partials/AboutMe.tsx
@@ -1,24 +1,40 @@
 import Image, { StaticImageData } from "next/image";
 import SectionTitle from "../ui/SectionTitle";
+import AboutMeImage from "@/public/assets/aboutme_image.png";
 
-type AboutMeProps = {
-    content: string;
-    imgSrc: StaticImageData;
-    imgAlt: string;
-}
+const AboutMe = () => {
+	return (
+		<section
+			id="about"
+			className="md:w-[70%] mx-auto scroll-m-20 flex flex-row border-2 border-background-lighter rounded-xl py-8 px-12 space-x-8"
+		>
+			<div className="md:w-[75%] min-h-full flex flex-col">
+				<SectionTitle title="About Me"></SectionTitle>
+				<div className="flex flex-col space-y-4">
+					<p className="font-roboto font-normal text-base text-foreground text-justify">
+						I’m Bayu, an IT student, aspiring developer, tech enthusiast, and most importantly, a lifelong learner. I’m
+						pursuing my degree in Information Technology at ITB STIKOM Bali, where I’m actively involved in the
+						International Volunteer and Programming Club. Beyond formal education, I’m a dedicated self-learner,
+						constantly seeking online resources to expand my knowledge and apply it to real-world projects. This drive
+						has led me to master creative software like Adobe Photoshop, Adobe Premiere Pro, and Figma, as well as
+						develop web apps using frameworks such as NextJS and TailwindCSS—tools I’ve used in creating my web
+						portfolio and a Timezone Converter app.
+					</p>
+					<p className="font-roboto font-normal text-base text-foreground text-justify">
+						Driven by my belief in learning a little bit of everything in tech, I’ve also cultivated an interest in
+						Machine Learning and Cybersecurity, which I find both fascinating and full of potential. My passion for
+						problem-solving extends to Competitive Programming on platforms like LeetCode, and it has sparked a growing
+						curiosity about Capture the Flag (CTF) challenges. As I continue to hone my skills in programming and
+						technology, I’m committed to exploring these fields further and making meaningful contributions in the
+						future.
+					</p>
+				</div>
+			</div>
+			<div className="md:w-[25%] flex flex-col justify-center items-center">
+				<Image className="object-cover rounded-xl" src={AboutMeImage} alt="placeholder image"></Image>
+			</div>
+		</section>
+	);
+};
 
-const AboutMe = (props: AboutMeProps) => {
-  return (
-    <section id="about" className="md:w-[70%] mx-auto scroll-m-20 flex flex-row border-2 border-background-lighter rounded-xl py-8 px-12 space-x-8">
-      <div className="md:w-[75%] min-h-full flex flex-col">
-        <SectionTitle title="About Me"></SectionTitle>
-        <p className="font-roboto font-normal text-base text-foreground text-justify">{props.content}</p>
-      </div>
-      <div className="md:w-[25%] flex flex-col justify-center items-center">
-        <Image className="object-cover rounded-xl" src={props.imgSrc} alt={props.imgAlt}></Image>
-      </div>
-    </section>
-  )
-}
-
-export default AboutMe
+export default AboutMe;
